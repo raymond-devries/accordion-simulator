9732101b941a1c0fd975720322aa854b3d9f606a
raymonddevries <raymond.l.devries@gmail.com>
2021-05-04 20:23:17 -0700

    Add dask implementation.

--- simulator.py
+++ simulator.py
@@ -1,6 +1,8 @@
+from dask import bag as dask_bag
 from core import SimulatorCore, console
 from typing import Optional
 from rich.progress import track
+from dask.distributed import Client
 
 
 def run_simulation(cost_per_deck: float,
@@ -28,27 +30,56 @@ def run_simulation(cost_per_deck: float,
 
 def run_multiple_simulations(cost_per_deck: float, earned_per_card: float,
                              simulations: int = 10, print_results: bool = False,
-                             print_games: bool = False):
-    simulator = SimulatorCore()
-    results = []
-    for _ in track(range(simulations), "Running Simulations"):
-        results.append(simulator.simulate(print_games))
+                             print_games: bool = False, use_dask: bool = False,
+                             dask_address: Optional[str] = None,
+                             dask_bag_partition_size: int = 10000,
+                             custom_dask_client: Optional[Client] = None) -> dict:
+    if use_dask:
+        if custom_dask_client is not None:
+            client = custom_dask_client
+            if dask_address:
+                console.print(
+                    f"[red]Since a custom dask client was provided, the dask address "
+                    f"of[/red] {dask_address} [red]is being ignored[/red]")
+        else:
+            client = Client(dask_address)
+        console.print(client)
+        client.upload_file('core.py')
+
+        if print_games:
+            console.print("[red]Printing games is disabled when using dask.[/red]")
+            console.print("[blue]Final results will still be printed[/blue]")
+
+        def simulate(*_):
+            return SimulatorCore().simulate()
+
+        bag = dask_bag.from_sequence(range(simulations),
+                                     partition_size=dask_bag_partition_size)
+        results = bag.map(simulate).compute()
+
+    else:
+        simulator = SimulatorCore()
+        results = [simulator.simulate(print_games) for _ in
+                   track(range(simulations), "Running Simulations",
+                         disable=print_games)]
 
     total_cards = sum(results)
+
     gross_total = total_cards * earned_per_card
     net_total = gross_total - cost_per_deck * simulations
 
     gross_average = gross_total / simulations
     net_average = net_total / simulations
 
-    if print_results:
+    if print_results or print_games:
         net_color = "red" if net_total < 0 else "green"
         console.rule("[b]RESULTS[/b]")
         console.print(f"You played {simulations: ,} games")
         console.print(f"Total gross earnings: [green]${gross_total: ,.2f}[/green]")
         console.print(
             f"Total net earnings: [{net_color}]${net_total: ,.2f}[/{net_color}]")
-        console.print(f"Average gross earnings: [green]${gross_average: ,.2f}[/green]")
+        console.print(
+            f"Average gross earnings: [green]${gross_average: ,.2f}[/green]")
         console.print(
             f"Average net earnings: [{net_color}]${net_average: ,.2f}[/{net_color}]")
 
