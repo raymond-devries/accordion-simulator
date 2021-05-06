f25da0f6071e1c36aef7e65c7ddda60cfa68e06e
raymond-devries <raymond.l.devries@gmail.com>
2021-05-06 14:52:23 -0700

    Add numba optimizations. Almost full magnitude of speedup!
    Working on a new algorithm to allow the entire simulation to be jitted.

--- accordion_simulator/core.py
+++ accordion_simulator/core.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import numpy as np
+from numba import njit
 from rich.console import Console
 
 VALUE = 0
@@ -35,6 +36,135 @@ SUIT_STYLES = {
 console = Console()
 
 
+def print_current_state(
+    game,
+    game_index,
+    card_being_checked: Optional[int] = None,
+    last_stacked: Optional[int] = None,
+):
+    total = game[0][TOTAL_CARDS]
+    index = 0
+    board = []
+
+    while total > 0:
+        value = PRETTY_VALUES[game[index][VALUE]]
+        suit = PRETTY_SUITS[game[index][SUIT]]
+        style = SUIT_STYLES[game[index][SUIT]]
+        total = game[index][TOTAL_CARDS]
+        selection = ""
+        stacked = ""
+        if card_being_checked == index:
+            selection = "[default on blue] [/default on blue]"
+        if last_stacked == index:
+            stacked = "[default on red] [/default on red]"
+        board.append(
+            f"{stacked}{selection}"
+            f"[{style}] {value}{suit} [/{style}]"
+            f"{selection}{stacked}"
+            f"([green]{total}[/green])"
+        )
+        index += 1
+        total = game[index][TOTAL_CARDS]
+
+    console.print(" ".join(board))
+    console.print(f"Number of active cards: {game_index + 1}")
+
+
+@njit
+def _compare(array: np.array, index1: int, index2: int):
+    return (
+        array[index1][VALUE] == array[index2][VALUE]
+        or array[index1][SUIT] == array[index2][SUIT]
+    )
+
+
+@njit
+def _move_card(array: np.array, old_index: int, new_index: int):
+    array[old_index][VALUE] = array[new_index][VALUE]
+    array[old_index][SUIT] = array[new_index][SUIT]
+    array[old_index][TOTAL_CARDS] += array[new_index][TOTAL_CARDS]
+
+    array[new_index][VALUE] = 0
+    array[new_index][SUIT] = 0
+    array[new_index][TOTAL_CARDS] = 0
+
+
+@njit
+def _shift(array, index):
+    card_total = array[index + 1][TOTAL_CARDS]
+
+    while card_total > 0:
+        _move_card(array, index, index + 1)
+        index += 1
+        card_total = array[index + 1][TOTAL_CARDS]
+
+
+@njit
+def _compare_replace(array, index) -> tuple[int, bool]:
+    if index >= 3:
+        if _compare(array, index, index - 3):
+            _move_card(array, index - 3, index)
+            _shift(array, index)
+            return index - 3, True
+    if index > 0:
+        if _compare(array, index, index - 1):
+            _move_card(array, index - 1, index)
+            _shift(array, index)
+            return index - 1, True
+
+    return index, False
+
+
+@njit
+def _move_deck_card_to_game(
+    old_array: np.array, new_array: np.array, deck_index: int, game_index: int
+):
+    new_array[game_index][VALUE] = old_array[deck_index][VALUE]
+    new_array[game_index][SUIT] = old_array[deck_index][SUIT]
+    new_array[game_index][TOTAL_CARDS] = 1
+
+
+@njit
+def _shuffle(array: np.array):
+    np.random.shuffle(array)
+
+
+# TODO fix function
+def _combine(array, index, game_index):
+    replace_index, replaced = _compare_replace(array, index)
+
+    if not replaced:
+        return game_index
+
+    game_index = game_index - 1
+    index = game_index
+
+    while replace_index <= index:
+        replace_index, replaced = _compare_replace(array, index)
+        if not replaced:
+            index = index - 1
+            continue
+
+        game_index = game_index - 1
+        index = game_index
+
+    return game_index
+
+
+# TODO fix function
+def simulate():
+    deck = np.array([(face, suit) for face in range(13) for suit in range(4)])
+    game = np.zeros((52, 3), np.uint8)
+    _shuffle(deck)
+    _move_deck_card_to_game(deck, game, 0, 0)
+    game_index = 1
+    for i in range(1, 52):
+        _move_deck_card_to_game(deck, game, i, game_index)
+        game_index = _combine(game, game_index, game_index) + 1
+
+    return game[0][TOTAL_CARDS]
+
+
 class SimulatorCore:
     def __init__(self):
         self.deck = np.array([(face, suit) for face in range(13) for suit in range(4)])
@@ -44,85 +174,6 @@ class SimulatorCore:
         self.game_number = 0
         self.game_results = None
 
-    def shuffle_cards(self):
-        np.random.shuffle(self.deck)
-
-    def move_deck_card_to_game(self, deck_index: int, game_index: int):
-        self.game[game_index][VALUE] = self.deck[deck_index][VALUE]
-        self.game[game_index][SUIT] = self.deck[deck_index][SUIT]
-        self.game[game_index][TOTAL_CARDS] = 1
-
-    def move_game_card(self, old_card_index: int, new_card_index: int):
-        self.game[old_card_index][VALUE] = self.game[new_card_index][VALUE]
-        self.game[old_card_index][SUIT] = self.game[new_card_index][SUIT]
-        self.game[old_card_index][TOTAL_CARDS] += self.game[new_card_index][TOTAL_CARDS]
-
-        self.game[new_card_index][VALUE] = 0
-        self.game[new_card_index][SUIT] = 0
-        self.game[new_card_index][TOTAL_CARDS] = 0
-
-    def compare_cards(self, index1: int, index2: int) -> bool:
-        return (
-            self.game[index1][VALUE] == self.game[index2][VALUE]
-            or self.game[index1][SUIT] == self.game[index2][SUIT]
-        )
-
-    def compare_replace(self, index) -> tuple[int, bool]:
-        if index >= 3:
-            if self.compare_cards(index, index - 3):
-                self.move_game_card(index - 3, index)
-                self.shift(index)
-                return index - 3, True
-        if index > 0:
-            if self.compare_cards(index, index - 1):
-                self.move_game_card(index - 1, index)
-                self.shift(index)
-                return index - 1, True
-
-        return index, False
-
-    def shift(self, index: int):
-        card_total = self.game[index + 1][TOTAL_CARDS]
-
-        while card_total > 0:
-            self.move_game_card(index, index + 1)
-            index += 1
-            card_total = self.game[index + 1][TOTAL_CARDS]
-
-        self.game_index -= 1
-
-    def print_current_state(
-        self,
-        card_being_checked: Optional[int] = None,
-        last_stacked: Optional[int] = None,
-    ):
-        total = self.game[0][TOTAL_CARDS]
-        index = 0
-        board = []
-
-        while total > 0:
-            value = PRETTY_VALUES[self.game[index][VALUE]]
-            suit = PRETTY_SUITS[self.game[index][SUIT]]
-            style = SUIT_STYLES[self.game[index][SUIT]]
-            total = self.game[index][TOTAL_CARDS]
-            selection = ""
-            stacked = ""
-            if card_being_checked == index:
-                selection = "[default on blue] [/default on blue]"
-            if last_stacked == index:
-                stacked = "[default on red] [/default on red]"
-            board.append(
-                f"{stacked}{selection}"
-                f"[{style}] {value}{suit} [/{style}]"
-                f"{selection}{stacked}"
-                f"([green]{total}[/green])"
-            )
-            index += 1
-            total = self.game[index][TOTAL_CARDS]
-
-        console.print(" ".join(board))
-        console.print(f"Number of active cards: {self.game_index + 1}")
-
     def combine(
         self,
         index: int,
@@ -131,13 +182,18 @@ class SimulatorCore:
         last_stacked: Optional[int] = None,
     ) -> int:
         if interactive:
-            self.print_current_state(
-                card_being_checked=index, last_stacked=last_stacked
+            print_current_state(
+                self.game,
+                self.game_index,
+                card_being_checked=index,
+                last_stacked=last_stacked,
             )
             input("Press enter to continue")
             print()
-        replace_index, replaced = self.compare_replace(index)
+        replace_index, replaced = _compare_replace(self.game, index)
+
         if replaced:
+            self.game_index -= 1
             return self.combine(
                 self.game_index, replace_index, interactive, replace_index
             )
@@ -153,9 +209,9 @@ class SimulatorCore:
                 index -= 1
                 continue
 
-            if index > 0 and self.compare_cards(index, index - 1):
+            if index > 0 and _compare(self.game, index, index - 1):
                 return False
-            if index >= 3 and self.compare_cards(index, index - 3):
+            if index >= 3 and _compare(self.game, index, index - 3):
                 return False
 
             return True
@@ -167,11 +223,12 @@ class SimulatorCore:
         save_csv: Optional[str] = False,
         deck_csv: Optional[str] = None,
     ):
+        self.game = np.zeros((52, 3), np.uint8)
         if deck_csv is not None:
             self.deck = np.genfromtxt(deck_csv, delimiter=",")
         else:
-            self.shuffle_cards()
-        self.move_deck_card_to_game(0, 0)
+            _shuffle(self.deck)
+        _move_deck_card_to_game(self.deck, self.game, 0, 0)
         self.game_index = 1
 
         if interactive:
@@ -203,7 +260,7 @@ class SimulatorCore:
 
         last_stacked = None
         for i in range(1, 52):
-            self.move_deck_card_to_game(i, self.game_index)
+            _move_deck_card_to_game(self.deck, self.game, i, self.game_index)
             if interactive:
                 console.print(f"[u]Cards left in deck: {51 - i}[u]")
             last_stacked = self.combine(
@@ -214,7 +271,7 @@ class SimulatorCore:
         self.game_index -= 1
 
         if print_game:
-            self.print_current_state()
+            print_current_state(self.game, self.game_index)
 
         if save_csv:
             if isinstance(save_csv, str):
